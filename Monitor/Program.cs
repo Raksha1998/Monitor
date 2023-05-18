14764af78603a75a8f25ef676bbf51493943a5fb
Raksha1998 <shreerakshadesai@gmail.com>
2023-05-18 14:34:21 +0200

    While loop to check if any key presses to stop

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -9,23 +9,30 @@ namespace Monitor
             //Initialising Variables
 
             string processName = "Notepad";// process name argument
-            int maxLifetimeMinutes = 2;// process maximum lifetime alloted
+            int maxLifetimeMinutes = 1;// process maximum lifetime alloted
             int monitoringFreqMin = 1;// process monitoring frequency
 
-            Console.WriteLine("Monioring process " + processName + "every " + monitoringFreqMin + " minutes and terminate if exceed "+maxLifetimeMinutes+" minutes...");
+            Console.WriteLine("Monioring process " + processName + " every " + monitoringFreqMin + " minutes and terminate if exceed "+maxLifetimeMinutes+" minutes...");
+            Console.WriteLine("Press any key to stop..");
+            
+            bool anyKeyPress = false;// boolean datatype to check if any key pressed
 
-            //loop to get each process in variable process and kill
-            foreach (var process in Process.GetProcessesByName(processName))
+            while (!anyKeyPress)// if not any key pressed continue...
             {
-                TimeSpan processlifetime = DateTime.Now - process.StartTime;//Calculate the pocess lifetime
-                if (processlifetime.Minutes > maxLifetimeMinutes)// condition to check if process lifetime exceeds the maximum lifetime
+                if (Console.KeyAvailable)//check if any key pressed
                 {
-                    process.Kill();//kill process
+                    anyKeyPress = true;//set any key press boolean datatype to true to exit from the loop
+                }
+                 //loop to get each process in variable process and kill
+                foreach (var process in Process.GetProcessesByName(processName))
+                {
+                    TimeSpan processlifetime = DateTime.Now - process.StartTime;//Calculate the pocess lifetime
+                    if (processlifetime.Minutes > maxLifetimeMinutes)// condition to check if process lifetime exceed
+                    {
+                        process.Kill();//kill process
+                    }
                 }
             }
-
-
-
         }
     }
 }
