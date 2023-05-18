795872dd3764b7b303a40b72eff1bb41de865f6e
Raksha1998 <shreerakshadesai@gmail.com>
2023-05-18 19:04:16 +0200

    Important-Removed cancel key press, exit condition will not get satisfied(Q-press)

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -12,63 +12,36 @@ namespace Monitor
             int maxLifetimeMinutes = 1;// process maximum lifetime alloted
             int monitoringFreqMin = 1;// process monitoring frequency
 
-            Console.WriteLine("Monioring process " + processName + " every " + monitoringFreqMin + " minutes and terminate if exceed "+maxLifetimeMinutes+" minutes...");
-            Console.WriteLine("Press any key to stop..");
-            try
+            Console.WriteLine("Monioring process " + processName + " every " + monitoringFreqMin + " minutes and terminate if exceed " + maxLifetimeMinutes + " minutes...");
+            Console.WriteLine("Press Q to stop..");
+
+
+            bool anyKeyPress = false;// boolean datatype to check if any key pressed
+            
+
+            while (!anyKeyPress)// if not any key pressed continue...
             {
-
-
-
-                bool anyKeyPress = false;// boolean datatype to check if any key pressed
-                bool cancelKeyPress = false;// boolean datatype to check if cancel key is pressed
-                Console.CancelKeyPress += (sender, eventArgs) =>
+                if (Console.KeyAvailable)//check if any key pressed
                 {
-                    eventArgs.Cancel = true;
-                    cancelKeyPress = true;
-                };//set cancel key press to true if process is closed or terminated
-
-
-
-                while (!anyKeyPress)// if not any key pressed continue...
-                {
-                    if (Console.KeyAvailable)//check if any key pressed
+                    ConsoleKeyInfo key_info = Console.ReadKey(intercept: true);//read the key and store in key_info
+                    if (key_info.Key == ConsoleKey.Q)// check if key pressed is Q
                     {
-                        ConsoleKeyInfo key_info = Console.ReadKey(intercept: true);//read the key and store in key_info
-                        if (key_info.Key == ConsoleKey.Q)// check if key pressed is Q
-                        {
-                            anyKeyPress = true;
-                            //Environment.Exit(0);
-                        }
-                    }
-
-                    while (!cancelKeyPress)//if not yet cancelled or terminated continue....
-                    {
-                        //loop to get each process in variable process and kill
-                        foreach (var process in Process.GetProcessesByName(processName))
-                        {
-                            TimeSpan processlifetime = DateTime.Now - process.StartTime;//Calculate the pocess lifetime
-                            if (processlifetime.Minutes >= maxLifetimeMinutes)// condition to check if process lifetime exceed
-                            {
-                                process.Kill();//kill process
-                                LogData(process.ProcessName);
-                            }
-                        }
-                        Thread sleepThread = new Thread(() => { Thread.Sleep(maxLifetimeMinutes * 60000); });
-                        sleepThread.Start();
-                        while (!Console.KeyAvailable)
-                        {
-                            Thread.Sleep(10);
-                        }
-                        sleepThread.Interrupt();
+                        anyKeyPress = true;
+                        //Environment.Exit(0);
                     }
                 }
+                //loop to get each process in variable process and kill
+                foreach (var process in Process.GetProcessesByName(processName))
+                {
+                    TimeSpan processlifetime = DateTime.Now - process.StartTime;//Calculate the pocess lifetime
+                    if (processlifetime.Minutes >= maxLifetimeMinutes)// condition to check if process lifetime exceed
+                    {
+                        process.Kill();//kill process
+                        LogData(process.ProcessName);
+                    }
+                }
+                Thread.Sleep(monitoringFreqMin * 60000);
             }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine("Application terminated..");
-            }
-           
         }
 
         static void LogData(string processName)
