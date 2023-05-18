b1d4e468d7449f8888edd34adcbf1a243e0a82cc
Raksha1998 <shreerakshadesai@gmail.com>
2023-05-18 11:36:41 +0200

    Initialised Variables

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialising Variables
+
+            string processName = "Notepad";// process name argument
+            int maxLifetimeMinutes = 2;// process maximum lifetime alloted
+            int monitoringFreqMin = 1;// process monitoring frequency
+        }
+    }
+}
