56fed830ce0767810a9b725e481c13da9725d973
Raksha1998 <shreerakshadesai@gmail.com>
2023-05-20 12:03:19 +0200

    Added Test case 2 with different assert

--- Monitor_Nunit_Test/UnitTest1.cs
+++ Monitor_Nunit_Test/UnitTest1.cs
@@ -8,18 +8,20 @@ namespace Monitor_Nunit_Test
 {
     public class Tests
     {
+        //Arrange            
+        string processName = "Notepad";
+        int maxLifetimeMinutes = 1;
+
+        
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
         public void TestCase_KillProcessRunningLong()
         {
-            //Arrange            
-            string processName = "Notepad";
-            int maxLifetimeMinutes = 1;
-
             //start a process
             Process.Start(processName);
 
@@ -31,6 +33,21 @@ namespace Monitor_Nunit_Test
             //Assert
             Process[] processempty = Process.GetProcessesByName(processName);
             Assert.IsEmpty(processempty);
+        }
+
+        public void TestCase2_KillProcessRunningLong()
+        {
+            //start a process
+            Process.Start(processName);
+
+            //Act
+            //wait for max life time minutes and 5 seconds more to ensure process exceeds maximum lifetime to kill
+            Thread.Sleep((maxLifetimeMinutes) * 65000);
+            ProcessMonitor.KillProcessRunningLong(processName, maxLifetimeMinutes);
+
+            //Assert
+            Process[] processempty = Process.GetProcessesByName(processName);
+            Assert.IsTrue(processempty.Length<0);
         }
     }
 }
